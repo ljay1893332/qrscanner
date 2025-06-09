2c9dd392ec693a0698e36616a364fd5b994a51c0
ljay1893332 <66163796+ljay1893332@users.noreply.github.com>
2025-06-09 13:20:29 +0800

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+export default defineConfig({
+  plugins: [svelte()],
+  base: '/qrscanner/'
+})
